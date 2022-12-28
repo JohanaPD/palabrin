4061173963f7a011caf6fbfbba6e64b116ae8bf2
JohanaPD <118165435+JohanaPD@users.noreply.github.com>
2022-12-28 11:40:19 +0100

    Update Funciones.java

--- src/Funciones/Funciones.java
+++ src/Funciones/Funciones.java
@@ -1,5 +1,38 @@
 package Funciones;
 
+import java.util.Scanner;
+
 public class Funciones {
-    String[] ArrayEscoger= new String[39];
+    public static int NUMERO_CARACTER = 8;
+    static Scanner sc = new Scanner(System.in);
+    String[] arrayEscoger = new String[31];
+    /*Pedir palabra*/
+
+    public String escogerPalabra() {
+        int aleatorio = (int) (Math.random() * 31 + 1);
+        String palabra = arrayEscoger[aleatorio];
+        return palabra;
+    }
+
+    public char[] convertirChar(String palabra, char[] arrayChar) {
+         arrayChar = new char[NUMERO_CARACTER];
+        arrayChar = palabra.toCharArray();
+        return arrayChar;
+    }
+
+    public String introducirPalabra() {
+        System.out.println("Introduzca una palabra real de  8 letras ");
+        String palabra;
+        do {
+            palabra = sc.nextLine();
+            palabra.trim().toUpperCase();
+            if (palabra.length() < NUMERO_CARACTER || palabra.length() > NUMERO_CARACTER) {
+                System.out.println("La palabra no tiene el número de letras solicitado");
+            }
+        } while(palabra.length() != NUMERO_CARACTER);
+        return palabra;
+    }
+
+
 }
+
