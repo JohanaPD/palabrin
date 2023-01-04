86d1496a6c9d958a98712da24e42ba5a0ac9dfd2
Johana <lara.crof.shs@gmail.com>
2023-01-04 10:15:05 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/modules.xml
    #	src/Main.java

--- src/Funciones/Funciones.java
+++ src/Funciones/Funciones.java
@@ -1,5 +1,45 @@
 package Funciones;
 
+import java.util.Scanner;
+
 public class Funciones {
-    String[] ArrayEscoger= new String[39];
+    public static int NUMERO_CARACTER = 8;
+    static Scanner sc = new Scanner(System.in);
+
+    public static String [] palabraDia= {"anatemas", "amuletos", "balbuceos", "baldosas", "censores", "centavos",
+            "distingo", "diversos", "escápula", "esférica", "factores", "faraones",
+            "Germania", "gestando", "halógeno", "haraposo", "inéditos", "inútiles",
+            "Jordania", "jornales", "kuwaitís", "káiseres", "lucrosos", "Luisiana",
+            "monarcas", "ocelotes", "racistas", "soñadora", "trajeran", "virreyes"
+    };
+
+    String[] arrayEscoger = new String[31];
+    /*Pedir palabra*/
+
+    public String escogerPalabra() {
+        int aleatorio = (int) (Math.random() * 31 + 1);
+        String palabra = arrayEscoger[aleatorio];
+        return palabra;
+    }
+
+    public char[] convertirChar(String palabra, char[] arrayChar) {
+         arrayChar = new char[NUMERO_CARACTER];
+        arrayChar = palabra.toCharArray();
+        return arrayChar;
+    }
+
+    public String introducirPalabra() {
+        System.out.println("Introduzca una palabra real de  8 letras ");
+        String palabra;
+        do {
+            palabra = sc.nextLine();
+            palabra.trim().toUpperCase();
+            if (palabra.length() < NUMERO_CARACTER || palabra.length() > NUMERO_CARACTER) {
+                System.out.println("La palabra no tiene el número de letras solicitado");
+            }
+        } while(palabra.length() != NUMERO_CARACTER);
+        return palabra;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import Funciones.Funciones;
 public class Main {
+    static Funciones usar= new Funciones();
     public static void main(String[] args) {
-
         System.out.println("cambios");
+        System.out.println(Funciones.palabraDia.length);
     }
 }
